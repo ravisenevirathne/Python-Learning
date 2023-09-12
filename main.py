ed918de4525ad58c25c65d31f7e1fdcb321f375b
ravisenevirathne <ravi2005@gmail.com>
2023-09-12 23:07:58 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# print("Hello"+" World")
+# print(5-2)
+# print(f"10 days are {10*24*60*60}  seconds")
+#
+
+calculations_to_seconds = 24 * 60 * 60
+name_of_unit = "seconds"
+print(f"10 days are {10 * calculations_to_seconds} {name_of_unit}")
