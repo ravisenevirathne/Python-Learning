4a15a51425198be354b5e10cd5b889ee746def4e
ravisenevirathne <ravi2005@gmail.com>
2023-09-13 19:40:22 +1000

    updated

--- main.py
+++ main.py
@@ -6,3 +6,4 @@
 calculations_to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 print(f"10 days are {10 * calculations_to_seconds} {name_of_unit}")
+print(f"20 days are {20 * calculations_to_seconds} {name_of_unit}")
